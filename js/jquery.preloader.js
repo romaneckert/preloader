a49862e4fadc296a0eabc4322ebd300c3ad4aec3
Roman Eckert <reckert@w-4.ch>
2014-10-26 08:11:14 +0100

    Test HTML Files

--- js/jquery.preloader.js
+++ js/jquery.preloader.js
@@ -2,7 +2,54 @@
  
     $.fn.preloader = function(config) {
     	
-    	return this;
+    	var elems = [];
+    	var tags = this;
+    	
+    	init();
+    	
+    	function init() {
+    		if(config == undefined) {
+    			config = {
+    				
+    			};
+    		}
+    		
+    		$(tags).each(function(t, tag) {
+    		
+    			$(tag).addClass('preloader');
+    			$('body').on('click', tag, handleTagClick);
+    		
+    			var elem = {
+	    			tag : tag,
+	    			content : undefined,
+	    			loaded : false,
+	    		};
+	    		
+	    		elems.push(elem);
+	    		
+	    	});
+    		
+    		load();
+    		
+    	};
+    	
+    	function handleTagClick(e) {
+    		e.preventDefault();
+    		
+    		
+    	};
+    	
+    	function load(elem) {
+    		$.each(elems, function(i, elem) {
+    			if(!elem.loaded) {
+    				console.log('not loaded');
+    				$( "#result" ).load( "ajax/test.html" );
+    				return false;
+    			}
+    		});
+    	};
+    	
+    	return tags;
     };
  
 }(jQuery));
