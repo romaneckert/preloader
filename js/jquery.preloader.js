9cc847a1acc3e16aca60e65193bf1653477b4e50
Roman Eckert <reckert@w-4.ch>
2014-11-12 21:44:08 +0100

    add simple preloading functions

--- js/jquery.preloader.js
+++ js/jquery.preloader.js
@@ -1,55 +1,103 @@
-(function ( $ ) { 
- 
+(function ( $ ) {  
+
     $.fn.preloader = function(config) {
     	
-    	var elems = [];
-    	var tags = this;
+    	var objects = [];
+    	var elems = this;
     	
     	init();
     	
     	function init() {
+    		
+    		if(!support) {
+    			return false;
+    		} else {
+    			$('html').addClass('preloader');
+    		}
+    		
     		if(config == undefined) {
     			config = {
     				
     			};
     		}
+
+    		$(elems).each(function(t, elem) {
     		
-    		$(tags).each(function(t, tag) {
-    		
-    			$(tag).addClass('preloader');
-    			$('body').on('click', tag, handleTagClick);
-    		
-    			var elem = {
-	    			tag : tag,
-	    			content : undefined,
+    			$(elem).addClass('preloader preloader-' + t);
+
+    			var object = {
+	    			elem : elem,
+	    			url : $(elem).attr('href'),
 	    			loaded : false,
 	    		};
 	    		
-	    		elems.push(elem);
+	    		objects.push(object);
 	    		
 	    	});
     		
-    		load();
+    		preload();
+    		
+    		$(elems).on('click', handleClick);
     		
     	};
     	
-    	function handleTagClick(e) {
+    	function support() {
+    		
+    		if (!(typeof (history.pushState) == "function")) {
+				return false;
+			}
+			
+			return true;
+    		
+    	};
+    	
+    	function handleClick(e) {
     		e.preventDefault();
+    		e.stopPropagation();
     		
+    		var elem = this;
     		
+    		$.each(objects, function(o, object) {
+    			if(object.elem == elem) {
+    				load(this);
+    			}
+    		});
+
     	};
     	
-    	function load(elem) {
-    		$.each(elems, function(i, elem) {
-    			if(!elem.loaded) {
-    				console.log('not loaded');
-    				$( "#result" ).load( "ajax/test.html" );
-    				return false;
+    	function load(object) {
+    		
+    		$('body').replaceWith(object.doc.body);
+    		$('title').text(object.doc.title);
+
+			var state = {
+				url : object.url
+			};
+			history.pushState(state, object.doc.title, object.url);
+
+    	};
+    	
+    	function preload() {
+    		$.each(objects, function(i, object) {
+    			if(!object.loaded) {
+
+    				$.ajax({
+						type : 'GET',
+						url : object.url,
+						dataType : 'html',
+					}).success(function(data) {
+			
+						object.doc = document.implementation.createHTMLDocument("html");
+						object.doc.documentElement.innerHTML = data;
+						
+						object.loaded = true;
+						
+					});
     			}
     		});
     	};
     	
-    	return tags;
+    	return elems;
     };
  
 }(jQuery));
